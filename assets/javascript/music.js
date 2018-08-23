dc327846f81a9a35dc5fdcdb975c963e3e20ee16
Jay Cha <kimchikimchi@gmail.com>
2018-08-23 16:57:00 -0400

    - Adding draft playlist. - Adding play-pause button. - Adding auto-play through playlist. - Adding current song lightlighting in the list

--- assets/javascript/music.js
+++ assets/javascript/music.js
@@ -7,6 +7,17 @@ curl --get --include 'https://deezerdevs-deezer.p.mashape.com/search?q=happy' \
   -H 'Accept: text/plain'
 */
 
+var playList = [];
+var audio = document.createElement("audio");
+var previousSongID = undefined;
+
+function drawSongList(song) {
+    var a = $('<a href="#" class="list-group-item list-group-item-action">');
+    a.attr('id', "song_" + song.id)
+    a.html(`${song.title} by ${song.artist_name}`);
+    $("#songs").append(a);
+}
+
 $.ajax({
     type: 'GET',
     url: 'https://deezerdevs-deezer.p.mashape.com/search',
@@ -15,14 +26,18 @@ $.ajax({
     },
     headers: {
         'X-Mashape-Key': 'X5MJlUMUCkmshf6HrBRNBXlx0XEzp1JPUi5jsnQAcws16kSuXo'
-    }
+    },
 }).then(function(response){
 //    console.log(response);
-
     var songlist = response.data;
+    var maxNumSongs = 10;
 
-    songlist.forEach(function(song){
-        var song_obj = {
+
+    // Limiting number of songs listed.
+    for (var index = 0; index < maxNumSongs; index++) {
+        var song = songlist[index];
+        var songObj = {
+                        id: song.id,
                         title : song.title,
                         preview_url: song.preview,
                         artist_name: song.artist.name,
@@ -30,7 +45,47 @@ $.ajax({
                         album_cover: song.album.cover_small,
                     };
 
-        console.log(song_obj);
-    });
+        //console.log(songObj);
 
+        // To Do: Render song list html
+        drawSongList(songObj);
+
+        playList.push(songObj);
+        // To Do: Add playlist to be played via HTML audio tag.
+        // makeSongPlayist(songObj);
+    }
 });
+
+
+// Based on audio play logic at https://stackoverflow.com/questions/8489710/play-an-audio-file-using-jquery-when-a-button-is-clicked
+
+function loadNextSong() {
+    var song = playList.shift();
+    audio.setAttribute("src", song.preview_url);
+
+    // Un-highlight previous song in the play list
+    $("#song_" + previousSongID).removeClass("active");
+    // Highlight the song in the song list.
+    $("#song_" + song.id).addClass("active");
+    previousSongID = song.id;
+}
+
+$("#playBtn").on("click", function(event) {
+    // When music is simply paused before,
+    // src will be populated
+    if ( audio.getAttribute('src') === null ) {
+        loadNextSong();
+    }
+
+    audio.play();
+});
+
+$("#pauseBtn").on("click", function(event) {
+    audio.pause();
+});
+
+// When the current song is over, play next
+audio.addEventListener('ended', function() {
+    loadNextSong();
+    this.play();
+}, false);
